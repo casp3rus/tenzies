c4bd28544eddb17718c57a5ce46c1dc0ccf9a795
Adrian M. Popan <adrian.popan@gmail.com>
2022-03-28 20:14:39 -0500

    add regular die face instead of numbers - css, grid template

--- src/components/Die.js
+++ src/components/Die.js
@@ -1,14 +1,23 @@
+import { nanoid } from 'nanoid';
+
 export default function Die(props) {
   const styles = {
     backgroundColor: props.isHeld ? '#59E391' : '#ffffff',
   }
+
+  const dieFace = []
+
+  for (let i = 1; i <= props.value; i++) {
+    dieFace.push(<span key={nanoid()} className='pip'></span>)
+  }
+
   return (
     <div
-      className='die'
+      className='face'
       style={styles}
       onClick={props.holdDie}
     >
-      <h1 className='die-num'>{props.value}</h1>
+      {dieFace}
     </div>
   );
 }
