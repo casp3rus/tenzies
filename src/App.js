7b5bec160e04bf88650fc74559856d5292a570ee
Adrian Popan <adrian.popan@gmail.com>
2022-03-31 03:13:07 -0500

    Fix the timer and the look of the stats for best game

--- src/App.js
+++ src/App.js
@@ -1,83 +1,95 @@
 import { useEffect, useState } from 'react';
-import './App.css';
-import Die from './components/Die';
-import Stats from './components/Stats'
 import { nanoid } from 'nanoid';
 import Confetti from 'react-confetti';
 
+import Die from './components/Die';
+import Stats from './components/Stats';
+
+import './App.css';
 
 function App() {
   const savedGame = JSON.parse(localStorage.getItem('bestGame'));
 
-  const bestGame = savedGame ? savedGame :
-    {
-      rolls: 0,
-      time: 0,
-    }
-  
-  console.log(bestGame)
+  const bestGame = savedGame
+    ? savedGame
+    : {
+        rolls: 0,
+        time: 0,
+      };
 
-  const newGame = {
-    tenzies: false,
-    rolls: 0,
-    time: 0,
-  }
-
-  const [dice, setDice] = useState(allNewDice())
-  const [game, setGame] = useState(newGame)
+  const [dice, setDice] = useState(allNewDice());
+  const [game, setGame] = useState(allNewGame());
 
   useEffect(() => {
-    dice.every((die) => die.isHeld && dice[0].value === die.value) ?
-      setGame(prevGame => ({ ...prevGame, tenzies: true })) :
-      console.log('Keep Rolling')
-  }, [dice])
+    dice.every((die) => die.isHeld && dice[0].value === die.value)
+      ? setGame((prevGame) => ({ ...prevGame, tenzies: true }))
+      : console.log('Keep Rolling');
+  }, [dice]);
+
+  useEffect(() => {
+    if (!game.tenzies) {
+      let interval = null;
+      interval = setInterval(() => {
+        setGame((prevGame) => ({ ...prevGame, time: prevGame.time + 1 }));
+      }, 1000);
+
+      return () => clearInterval(interval);
+    }
+  }, [game.time]);
 
   function allNewDice() {
-    return Array.from({ length: 10 }, () => generateNewDie())
+    return Array.from({ length: 10 }, () => generateNewDie());
+  }
+
+  function allNewGame() {
+    return {
+      tenzies: false,
+      rolls: 0,
+      time: 0,
+    };
   }
 
   function generateNewDie() {
     return {
       id: nanoid(),
       value: Math.ceil(Math.random() * 6),
-      isHeld: false
+      isHeld: false,
+    };
+  }
+
+  function saveBestRoll() {
+    if (bestGame.rolls > game.rolls || bestGame.rolls === 0) {
+      bestGame.rolls = game.rolls;
     }
   }
 
-  useEffect(() => {
-    if (!game.tenzies) {
-      const startTime = Date.now()
-      setTimeout(() => setGame(oldGame => ({ ...oldGame, time: Math.floor(oldGame.time + (Date.now() - startTime) / 1000) })), 1000)
-    } else {
-      // saveBestTime()
+  function saveBestTime() {
+    if (bestGame.time > game.time || bestGame.time === 0) {
+      bestGame.time = game.time;
     }
-  }, [game.time])
-
-  function saveGameToLocalStorage() {
-    if (bestGame.rolls > game.rolls || bestGame.rolls === 0) {
-      bestGame.rolls = game.rolls
-    } else if (bestGame.time > game.time || bestGame.time === 0) {
-      bestGame.time = game.time
-    }
-
-    localStorage.setItem('bestGame', JSON.stringify(bestGame))
   }
 
   function rollDice() {
     if (!game.tenzies) {
-      setDice(prevDice => prevDice.map(die => die.isHeld ? die : generateNewDie()))
-      setGame(prevGame => ({ ...prevGame, rolls: prevGame.rolls + 1 }))
+      setDice((prevDice) =>
+        prevDice.map((die) => (die.isHeld ? die : generateNewDie()))
+      );
+      setGame((prevGame) => ({ ...prevGame, rolls: prevGame.rolls + 1 }));
     } else {
-      saveGameToLocalStorage()
-      setGame(newGame)
-      setDice(allNewDice())
+      saveBestRoll();
+      saveBestTime();
+      localStorage.setItem('bestGame', JSON.stringify(bestGame));
+      setGame(allNewGame());
+      setDice(allNewDice());
     }
   }
 
   function holdDie(id) {
-    setDice(prevDice => prevDice.map(die => {
-      return die.id === id ? { ...die, isHeld: !die.isHeld } : die
-    }))
+    setDice((prevDice) =>
+      prevDice.map((die) => {
+        return die.id === id ? { ...die, isHeld: !die.isHeld } : die;
+      })
+    );
   }
 
   const diceElements = dice.map((die) => (
@@ -87,22 +99,20 @@ function App() {
       isHeld={die.isHeld}
       holdDie={() => holdDie(die.id)}
     />
-  ))
+  ));
 
   return (
     <main>
       {game.tenzies && <Confetti />}
       <h1 className="title">Tenzies</h1>
-      <p className="instructions">Roll until all dice are the same. Click each die to freeze it at it's current value between rolls.</p>
+      <p className="instructions">
+        Roll until all dice are the same. Click each die to freeze it at it's
+        current value between rolls.
+      </p>
       <Stats title={'Best'} rolls={bestGame.rolls} time={bestGame.time} />
       <Stats title={'Current'} rolls={game.rolls} time={game.time} />
-      <div className="dice-container">
-        {diceElements}
-      </div>
-      <button
-        className='roll-button'
-        onClick={rollDice}
-      >
+      <div className="dice-container">{diceElements}</div>
+      <button className="roll-button" onClick={rollDice}>
         {game.tenzies ? 'new game' : 'roll'}
       </button>
     </main>
--- src/components/Stats.js
+++ src/components/Stats.js
@@ -1,11 +1,11 @@
 export default function Stats({ rolls, title, time }) {
   return (
     <div>
-      <h1 className='stats'>{`${title} Game`}</h1>
-      <div className='stats--details'>
+      <h1 className="stats">{`${title} Game ${title === 'Best' ? 'Ever' : ''}`}</h1>
+      <div className="stats--details">
         <span>ROLLS:{rolls}</span>
         <span>{title === 'Current' ? `${time}s` : `TIME:${time}s`}</span>
       </div>
     </div>
-  )
+  );
 }
